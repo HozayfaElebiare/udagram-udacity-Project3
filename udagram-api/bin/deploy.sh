75babe12d83e16a884d3863397ecd7e754fe186c
Hozayfa Elebiare <33654362+HozayfaElebiare@users.noreply.github.com>
2022-03-12 20:55:24 +0200

    first commit

---
+++ udagram-api/bin/deploy.sh
@@ -0,0 +1,5 @@
+cd www
+echo start deploing $AWS_REGION
+eb init udacityClass1 --region us-east-1 --tags name=udacityClass1
+eb deploy $EB_APP
+eb setenv POSTGRES_USERNAME=postgress POSTGRES_PASSWORD=abcd1234 POSTGRES_DB=postgres PORT=5432 POSTGRES_HOST=udagrambd.cpqqlc4nvh4m.us-east-1.rds.amazonaws.com AWS_REGION=us-east-1 AWS_PROFILE=hozyfa AWS_BUCKET=s3.udacity.class1 URL=http://udacityeb.us-east-1.elasticbeanstalk.com/ JWT_SECRET=helloworld
